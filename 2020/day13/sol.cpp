ccd874e8d872d3f816e33c655ab105e2701b99d4
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-03-21 17:21:10 +0100

    day 13 part 2 in c++
     - Merged parts 1 and 2 into one file `sol.cpp`

---
+++ 2020/day13/sol.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using u64 = uint64_t;
+
+std::vector<std::string> split_string (const std::string& target, char delimeter = ' ') {
+    std::vector<std::string> out;
+    size_t last_delimeter = 0;
+
+    while (true) {
+        auto i = target.find(delimeter, last_delimeter);
+        if (i == std::string::npos) {
+            out.push_back(target.substr(last_delimeter));
+            break;
+        }
+        out.push_back(target.substr(last_delimeter, i - last_delimeter));
+        last_delimeter = i + 1;
+    }
+
+    return out;
+}
+
+int main(){
+    std::ifstream input("input");
+
+    u64 earliest;
+    std::string line;
+
+    input >> earliest;
+    input >> line;
+
+    input.close();
+
+    auto timings = split_string(line, ',');
+    std::vector<std::pair<size_t, u64>> buses;
+    for (size_t i = 0; i < timings.size(); ++i) {
+        if(timings[i] != "x") {
+            buses.emplace_back(i, stoull(timings[i]));
+        }
+    }
+
+    u64 min = UINT64_MAX;    /* 2^64 - 1 */
+    u64 nearest;
+    u64 first_bus = 0;
+    for (const auto& bus : buses) {
+        auto id = bus.second;
+        nearest = earliest / id * id + id;
+        if (nearest < min) {
+            min = nearest;
+            first_bus = id;
+        }
+    }
+
+    std::cout << "part 1: " << first_bus * (min - earliest) << '\n';
+
+    u64 time = 0;
+    u64 step = buses[0].second;
+
+    buses.erase(buses.begin());
+    for (const auto& bus : buses) {
+        auto i = bus.first;
+        auto id = bus.second;
+        while ((time + i) % id != 0) {
+            time += step;
+        }
+        step *= id;
+    }
+
+    std::cout << "part 2: " << time << '\n';
+
+    return 0;
+}

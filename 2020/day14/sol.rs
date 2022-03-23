6b33f315b8d16fcc1be92e11a1813165a6a52547
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-03-23 20:48:28 +0100

    day 14: riir! part 2

--- 2020/day14/sol.rs
+++ 2020/day14/sol.rs
@@ -6,6 +6,7 @@ fn main() {
 
     let mut memory: HashMap<u64, u64> = HashMap::new();
     let mut mask: String = String::new();
+    let mut program: Vec<_> = Vec::new();
 
     input.lines().by_ref().for_each(|line| {
         if line.starts_with("mask") {
@@ -20,6 +21,7 @@ fn main() {
                 .map(|(a, v)| (a.parse::<u64>().unwrap(), v.parse::<u64>().unwrap()))
                 .unwrap();
 
+            program.push((address, value, mask.clone()));
             memory.insert(address, apply_mask(value, &mask));
         }
     });
@@ -27,6 +29,14 @@ fn main() {
     let memory_sum: u64 = memory.values().sum();
 
     println!("part 1: {}", memory_sum);
+
+    memory.clear();
+
+    program.iter().for_each(|(address, value, m)| {
+        fill_memory(&mut memory, *address, *value, 36, m);
+    });
+
+    println!("part 2: {}", memory.values().sum::<u64>());
 }
 
 fn apply_mask(value: u64, mask: &str) -> u64 {
@@ -39,4 +49,41 @@ fn apply_mask(value: u64, mask: &str) -> u64 {
     });
 
     masked_value
+}
+
+fn fill_memory(memory: &mut HashMap<u64, u64>, address: u64, value: u64, index: usize, mask: &str) {
+    if index == 0 {
+        memory.insert(address, value);
+        return;
+    }
+
+    let next_bit = mask.chars().nth(index - 1).unwrap();
+
+    match next_bit {
+        '0' => fill_memory(memory, address, value, index - 1, mask),
+        '1' => fill_memory(
+            memory,
+            address | (1 << (36 - index)),
+            value,
+            index - 1,
+            mask,
+        ),
+        'X' => {
+            fill_memory(
+                memory,
+                address | (1 << (36 - index)),
+                value,
+                index - 1,
+                mask,
+            );
+            fill_memory(
+                memory,
+                address & !(1 << (36 - index)),
+                value,
+                index - 1,
+                mask,
+            );
+        }
+        _ => {}
+    }
 }

cb4f89b964c09e201270781189ed3ed31d565c99
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-05-04 22:44:54 +0200

    day 16: update part 1 a little bit

--- 2020/day16/part1.cpp
+++ 2020/day16/part1.cpp
@@ -1,51 +1,42 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
 
-typedef struct {
+struct Range {
     int min1,
-        max1,
-        min2,
-        max2;
+    max1,
+    min2,
+    max2;
 
-    bool in_range(int num){
+    bool in_range(int num) const {
         return (min1 <= num && num <= max1) || (min2 <= num && num <= max2);
     }
-} Range;
+};
 
-std::vector<std::string> split_string(std::string target, char delimeter = ' '){
-    std::vector<std::string> out;
-    int position = 0;
-    size_t length = target.length();
-
-    for(size_t i = 0; i < length; i++){
-        if(target[i] == delimeter){
-            out.push_back(std::string(target, position, i - position));
-            position = i+1;
-            continue;
-        }
-        if(i == length - 1){
-            out.push_back(std::string(target, position, length - position));
-        }
+std::vector<std::string> split_string(std::string s, const std::string delim, std::vector<std::string> acc = {}) {
+    auto pos = s.find(delim);
+    if (pos != std::string::npos) {
+        acc.push_back(s.substr(0, pos));
+        return split_string(s.substr(pos + delim.length()), delim, acc);
+    } else {
+        acc.push_back(s);
+        return acc;
     }
-
-    return out;
 }
 
 int main(){
-
-    std::string my_ticket;
-    uint32_t error_rate = 0;
+    std::ifstream input("input");
     std::vector<Range> notes_fields;
 
-    for(size_t i = 0; i < 20; i++){
+    for (size_t i = 0; i < 20; i++) {
         std::string line;
-        std::getline(std::cin, line);
+        std::getline(input, line);
 
-        auto line_splitted = split_string(line);
+        auto line_splitted = split_string(line, " ");
         size_t line_items = line_splitted.size();
 
-        auto range1 = split_string(line_splitted[line_items - 3], '-');
-        auto range2 = split_string(line_splitted[line_items - 1], '-');
+        auto range1 = split_string(line_splitted[line_items - 3], "-");
+        auto range2 = split_string(line_splitted[line_items - 1], "-");
 
         int from1 = std::stoi(range1[0]);
         int to1 = std::stoi(range1[1]);
@@ -53,33 +44,37 @@ int main(){
         int from2 = std::stoi(range2[0]);
         int to2 = std::stoi(range2[1]);
 
-        Range field = {};
-        field.min1 = from1;
-        field.min2 = from2;
-        field.max1 = to1;
-        field.max2 = to2;
+        Range field {
+            from1,
+            to1,
+            from2,
+            to2
+        };
 
         notes_fields.push_back(field);
     }
 
-    std::cin.ignore();
-    std::cin.ignore(256, '\n');
-    std::getline(std::cin, my_ticket);
+    input.ignore();
+    input.ignore(256, '\n');
 
-    std::cin.ignore();
-    std::cin.ignore(256, '\n');
+    std::string my_ticket;
+    std::getline(input, my_ticket);
+
+    input.ignore();
+    input.ignore(256, '\n');
+
+    uint32_t error_rate = 0;
 
     std::string nearby_ticket;
-    for(size_t i = 0; i < 241; i++){
-        std::cin >> nearby_ticket;
-        auto nearby_ticket_fields = split_string(nearby_ticket, ',');
+    while (std::getline(input, nearby_ticket)) {
+        auto nearby_ticket_fields = split_string(nearby_ticket, ",");
 
-        for(auto &x : nearby_ticket_fields){
+        for (auto &x : nearby_ticket_fields) {
             bool in_range = false;
             int current_field = std::stoi(x);
-            for(auto &field : notes_fields){    /* bad variable naming */
-                if(field.in_range(current_field)){
-                        in_range = true;
+            for (auto &field : notes_fields) {    /* bad variable naming */
+                if (field.in_range(current_field)) {
+                    in_range = true;
                 }
             }
             if (!in_range) {

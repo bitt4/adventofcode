f54a308956b3d8a56baa46f36284d16793b1182a
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-05-25 17:38:08 +0200

    day 14: formatting, style, etc.

--- 2020/day14/part1.cpp
+++ 2020/day14/part1.cpp
@@ -1,21 +1,25 @@
+#include <fstream>
 #include <iostream>
 #include <map>
 
-uint32_t get_address(std::string mem){
+using u64 = uint64_t;
+using u32 = uint32_t;
+
+u32 get_address(const std::string& mem) {
     size_t i = 4;
-    while(mem[i] != ']')
+    while (mem[i] != ']') {
         i++;
-    return static_cast<uint32_t>(std::stoul(mem.substr(5 - 1, 5 + i)));
+    }
+    return std::stoul(mem.substr(5 - 1, 5 + i));
 }
 
-uint64_t apply_mask(std::string mask, uint64_t num){
-    uint64_t masked = num;
-    for(size_t i = 0; i < 36; i++){
-        if(mask[i] != 'X'){
-            if(mask[i] == '1'){
+u64 apply_mask(const std::string& mask, const u64 num) {
+    u64 masked = num;
+    for (size_t i = 0; i < 36; ++i) {
+        if (mask[i] != 'X') {
+            if (mask[i] == '1') {
                 masked |= (1UL << (35 - i));
-            }
-            else {
+            } else {
                 masked &= ~(1UL << (35 - i));
             }
         }
@@ -24,30 +28,27 @@ uint64_t apply_mask(std::string mask, uint64_t num){
     return masked;
 }
 
-int main(){
+int main() {
+    std::ifstream input("input");
+    std::map<u32, u64> memory;    /* I think memory addresses could in this case be 16-bit, but whatever */
 
     std::string mask;
-    uint64_t sum = 0;
-    std::map<uint32_t, uint64_t> memory;    /* I think memory addresses could in this case be 16-bit, but whatever */
+    std::string line;
+    while (std::getline(input, line)) {
+        std::string var = line.substr(0, line.find(' '));
+        std::string value = line.substr(line.rfind(' ') + 1);
 
-    const size_t input_size = 585;
-
-    std::string var, value;
-    for(size_t i = 0; i < input_size; i++){
-        std::cin >> var;
-        std::cin.ignore(3);
-        std::cin >> value;
-
-        if(var == "mask")
+        if (var == "mask") {
             mask = value;
-        else {
-            uint32_t current_mem = get_address(var);
+        } else {
+            u32 current_mem = get_address(var);
             memory[current_mem] = apply_mask(mask, std::stoull(value));
         }
     }
 
-    for(const auto &x : memory){
-        sum += x.second;
+    u64 sum = 0;
+    for (const auto& [_, x] : memory) {
+        sum += x;
     }
 
     std::cout << sum << '\n';

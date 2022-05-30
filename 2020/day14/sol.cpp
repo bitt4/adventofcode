36869690598df9bf6454fee0152f738d48c37592
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-05-30 22:44:09 +0200

    day 14: part 2 in c++

--- 2020/day14/sol.cpp
+++ 2020/day14/sol.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
 
 using u64 = uint64_t;
 using u32 = uint32_t;
@@ -28,30 +29,69 @@ u64 apply_mask(const std::string& mask, const u64 num) {
     return masked;
 }
 
+void fill_memory(std::map<u64, u64>& memory, u64 address, u64 value, std::string mask, u32 i = 36) {
+    if (i == 0) {
+        memory[address] = value;
+        return;
+    }
+
+    auto next_bit = mask[i - 1];
+
+    switch (next_bit) {
+    case '0':
+        fill_memory(memory, address, value, mask, i - 1);
+        break;
+    case '1':
+        fill_memory(memory, address |  (1ULL << (36 - i)), value, mask, i - 1);
+        break;
+    case 'X':
+        fill_memory(memory, address |  (1ULL << (36 - i)), value, mask, i - 1);
+        fill_memory(memory, address & ~(1ULL << (36 - i)), value, mask, i - 1);
+        break;
+    default: {}
+    }
+}
+
 int main() {
     std::ifstream input("input");
-    std::map<u32, u64> memory;    /* I think memory addresses could in this case be 16-bit, but whatever */
+    std::map<u64, u64> memory;
+    std::vector<std::tuple<u64, u64, std::string>> program;
 
     std::string mask;
     std::string line;
     while (std::getline(input, line)) {
         std::string var = line.substr(0, line.find(' '));
-        std::string value = line.substr(line.rfind(' ') + 1);
+        std::string arg = line.substr(line.rfind(' ') + 1);
 
         if (var == "mask") {
-            mask = value;
+            mask = arg;
         } else {
             u32 current_mem = get_address(var);
-            memory[current_mem] = apply_mask(mask, std::stoull(value));
+            u64 value = std::stoull(arg);
+            memory[current_mem] = apply_mask(mask, value);
+            program.push_back({ current_mem, value, mask });
         }
     }
 
-    u64 sum = 0;
+    u64 sum1 = 0;
     for (const auto& [_, x] : memory) {
-        sum += x;
+        sum1 += x;
     }
 
-    std::cout << sum << '\n';
+    std::cout << "part 1: " << sum1 << '\n';
+
+    memory.clear();
+
+    for (auto [address, value, mask] : program) {
+        fill_memory(memory, address, value, mask);
+    }
+
+    u64 sum2 = 0;
+    for (const auto& [_, x] : memory) {
+        sum2 += x;
+    }
+
+    std::cout << "part 2: " << sum2 << '\n';
 
     return 0;
 }

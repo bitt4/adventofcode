1f37d76a74225c148b9ab24228f34f60b27f0206
bitt4 <64093718+bitt4@users.noreply.github.com>
2022-08-12 01:02:34 +0200

    day 19: part 1 and part 2

---
+++ 2020/day19/sol.cpp
@@ -0,0 +1,220 @@
+/*
+                                                                              `..`
+                                                                         `.+xMxnnxxz*,.`
+                                                                    `.:;+xWzi:zi*#+zzxMMn+:.
+                                                                 :*#M@nnzxxWnnzn##+zz#z###nWMi`
+                                                               `#nzxM##z##*#xnMnzMz++#+*++*++zM*.
+                                                               #z*nnx##+##xMx+i*zMzxxz*z##++z**xW+.
+                                                             `+M+zzWz+++i#xnzizMW*n#z+#+++*++#+;:+x+`
+                                                            ixMxn#Wnx++zxzznMnxWMMMnn**:i+#+z#i**,.+x;
+                                                           :Mz#xMMMzxMx+##n@MnxnnxzzzzMx#+z#nn+:,,i:;n*
+                                                           znx#@MWxM@xznxMMWMxzxM@W+zi;*#+x*i*###*#,:;n#
+                                                          :WnxnM@nnnMWx#MxzxxMWWWnzxz##zxz*#nx+*+zz#+iix+
+                                                          +xx+Mxx@n@nnxMxxx#xnx##z+zxzzi+;+zi##xx+i*+;+;x;
+                                                         ;@xMnWWM@MxxWWM@Wnn@Mzxzz#n#zxxn++*#znnn##**;,:;M,
+                                                        .x@xn@MM@MnnM@zn#nxn+i#nxzWz+#++++#++**nnWM*:i;::ix,
+                                                        nzMnxxMW@xxxWWxizMx+nnz#iz+*nnz+#*z#*z:i+++#+++zi,ix`
+                                                       .xnMz@MxW@nWMM+nn#zzMWnnxnzxnnz#z+**+nnzx*,iz+;:+n+;#n`
+                                                       *#nzxWxnW@M@xxWxnxzzxx##xnnznxnxxxx#n+ii*+nM+:.,:i++;nz
+                                                       MznMxWx@WW@WnWMzx+#zx+nznMM+#++i:.:#nxxxz##*nz*;;izzxzz*
+                                                      ;M+zxz@MzMWMWxn+n#nz#x#n#nnMMMWxxM#n+z*,;zzznxWxz;;ii#+;n;
+                                                     `Mx+xMn@WW@z@MMx+MMn*izMzzz#nz+#znxnnnx#nzxn#zn#*i+x#iix+iM`
+                                                     zn*nMWn@xWWMMx@WxMzzMxxzznx#MMnnnz#nzii#z#n+nz#z#*iizn*###z*
+                                                     M*innMnxWxMzxMWzWxxzxnxn#MnMnnxnxnnnni*#*++n*+*z#nxnz+;:*xzx`
+                                                    .W+#xnMnM@WMMWMMnnWWWMnMMzzx##+nzz#@Mnzxx##**zxx+#i;+zM+ii*#x,
+                                                    ;n*+MxxMWWMnx@xnMxWxMzzMWxnn@xxn#+MnxznnzMnii*n#WWn#i;*ii+i*##
+                                                    xW;x#xMM@WzWMxnMxx@xWxn+MMMn#M*z#nnnM#nnn#nxxnzzxz#nxnxzi;:i*x
+                                                   .M:#Wz+M@@WWnMnnnMxxxnxWW@WnMWxxxMWWWWz+i;+nz#xWM@@z*;+z;#i::iW.
+                                                   iz,*@#xW@W@MnzMMzMx@MMMMWWWn+#nnnMM++i*zxnn#zn##*+###nn+;:##,:zi
+                                                   ziziz*WM@@WMMxWWn@@MxMWMMMxW@@Wn@WMxxWM#i*xx#zznzxz#;*xni:,;;*z+
+                                                  `Wznin+xMW@WMW@@WxxMMMMMxnxxWWWMWMMWMxz+##zi*nnz+i*+izz**ni:;ii#+
+                                                  .@W;nn+nxx@Wn@WWWMMWMM@Mz+WznnMMMx#xMxMMWnxWMxznMzn##i.z#.+i,;;n#
+                                                  *WMinn+Mzn@@MWMxMMWW@MnxxxMW@xxnMzx#WxMxn#zMx@@M+z*z#zz+*+:i::;+n
+                                                  ;Wz+#znMxn@x@@MWMM@WxMzMMzMxxWn+#i+z+zWMnz#*###MMWxxxzxx#,i,i:i#z
+                                                  ixxnz#zn@zMMWW@@MM@MWMnWWxx#zxxzn##x*nxMxMMn@xz+#z+xMzx@##;,;i+#i
+                                                  ++M#x*xzx*WWWWWnWM@MMWW@xxnxMMnxxxMMzn##nxizWnxnn#z*n@Wx#;;:.:*Wi
+                                                  #nx:x*xWM#xW@@#@@W@@@WW@WMxxM@@@MWMWnWWxWMnMMMMMxnxWMx##z**n,*,*z
+                                                  ,Wni##Wxnzn@@M@@nx@x@@WW#Wx@########@@@@nM+zzn+#+#n*;i;+#z+##i*:x`
+                                                   M+#niM#xnzWWW@@WMW@xMW@@##############@@WWM#zz#niMz#z#n;###M#:;n:
+                                                   Mz#x.xMMz#MMM@@WMWMzW@@W##############@WWxzWzn*+MWn+n+xMin#z*i:x:
+                                                   x#x+*Mnxxn#MMWMWx@MxWx@@##############@WMxz#zi#xxMWMWnznxzMin;i#;
+                                                   xnWn+nxWnMzzxxxMz@MWW@################WWMnMz+##+MM@@@WxMnxnzn*#+i
+                                                   #xz#zzxxnxnM#+zMMWW@W@################@@MWMM++ziM@@##@@WMnxxx*nx,
+                                                   ##z+#*Mnnx#nz+MxxxWMxWW@#############W@W@Wxzzz+xM@######@zM+x*xM`
+                                                   iznni+znnM#z*zzx@xWnz#W@###############@@Mnx+##x@@########xznxzM`
+                                                   `WMzinWMn#M+*MznW@@#MMMW@############@@WWM#n+*xW##########@z+Mn@.
+                                                    Mxn*nW#MnnzxznxM@WzWzMW#############@WW@xM+z#xM###########WMnnx
+                                                    :@n+xnzMn#M@+zzMMxW#MxW#############@@@@Mx###n@###########W@xx;
+                                                    `WnzzMxnxnW@#xx@WW@WMW@#############@xW@Mz*+zx############MW#x`
+                                                     nM*zMWznnWMxznW@MWWWWM####W@#######@@@#W#+i*n############W###
+                                                     :M*#x@+xn@#nzWxWWWMnxMWxMWWW#######@@#WMzn#+W###########@Wxn;
+                                                     ,Mz#Mxn@MW#xznnMMWxz#zz+*;#@@W@#@##@W@WWn+*z@###########xz#x.
+                                                     .WzMWz#xM@z**Wzx@zx#nn#iizMnMnWMM###WWW@zi:W@###########zx*n*
+                                                     `Wix@n#xM@M#+x#xWMxxx##z#nn*zzMx@@@WWWWz+#*x@###@@#####MMx+*M`
+                                                     `W;xW++nn#M+*nxxWMWWMxi;;,+#x+M@@@@WWWznzx#x@#@@@@M@W@M@#niiW`
+                                                     ;xinnnMnn@z#ixzMx@MMWz+*i##n*xMWMWWWMxxM++++@@nM@x@nxxnM#z*;W`
+                                                     *zzi#Mn+xW##nM*WMW@MM#nznzMMxxWxMMMMzMMM*.,iM@x+M@WMMW@n*+*;M`
+                                                     n+#nxxMzMWx++*#zxW@MMMMnnW@MxMM@W@z#zWMW#**i#MxWMzMnzzMz+*++M`
+                                                    `Wi##n#zMWWWn+++zx@W@xnzxMxx+nMWMMWznnMnWzzxzMMnnni#+*++*:ii##
+                                                    ;W*+xn*xW@@Wz*#nzxWWWxzM@W#zM;MWz#zxWMMnMnMn+nzWi+zi##z**;,ini
+                                                    iM;xnxnx@@WMWnznx*M@nMnMnxnMxzx@MWWWW@WWxM@xMzzWMz****#+++;zM,
+                                                    #*+#zM#nWWxxWn#n#zzWnnxn##+x#MWx@@MzzxMxWMMznxMMnn+*+i;+**:@M`
+                                                    x+nxnWxx@M@x@Wnn+znMx#WzznxxMMWMM@@@@@@M@WnxMWW#xzi#zi#+,i*W*
+                                                   ,WzxxnnxxnM@Wn@Mzn+zM+zWxMMMWnWWWxMxx@W@W@W@xxMzxz+#n*z+iiini`
+                                                   #z*MMWnnWnWxnnx@Mz+zx#zMnMMxxWMznMMMMWMzWWx@MMMxz*:*#z#;i+;M`
+                                                  ;x*+zWxxxWx@nnxMWW+*zxn#nnW#MW@WzzMW@M@@MMxnWnxxnn+#i*+#i+zM;
+                                                 `xz*++xznxxM@MxWMWWz#+nz#MWWzW+#xxxMWMW@@xMMi++MWM##*i##n;#M:
+                                                 *Mnz#+nMxMMWM@@nMMWW+zMznnxxMMnWMnM#nnxnnx**##xMnMx+*izMxnnz
+                                                 n++i+nxzMMxMMW@@xxx#@zxMnnnzMn#nn@WWnzn*+#z#z#*zWMn***xM*zM`
+                                                .Wzz##zWMnWWz@@Mxxxn@@W#MnWnnW#+#zxM@Wzn+#zxn##i#Wxzxi#i#nn,
+                                               .zWni+nzWnx@MW@MxWWWxx@@MMnWxxxx**n#nnzxWMx#x*nzi+xM#z;+zzW,
+                                              `xn#n#+MMMnMWnMMxWWMnMz@@MWMxnz#ni*+zzzxn+++MWMMnz#zxnz**xn,
+                                             `nM#M*+MnMMnWMWnMWW@MM@WW@xWxWnznnznnWxnxMMz#i*zMnW*nMz*#iM`
+                                             +Mn;*##+xxzMMWzMzMMxx@WWMM@WWnMzzz#xMxnxW@WW@@Wn*+x*#*#**M*
+                                            iWz**nz+MxM+xWxMMxnMWM@W@WnWMWMnx#xxzMxMM+xMn+M#n+*i*+++#nW`
+                                          `#x##nnz#nM##+MW@@nWMMWM@WMW@W##@#Wn+xnznW@@WxWn*#+#*i##z;iW#
+                                     ```.nxzzi#zxz+n#zxWMx@WWMWMx#n#WWW@##M@nM#+nxM#ziz#xWxzz+z##xz;*@;
+                                `;*#zMWxWM+*+*znWzxnzz@MnnWWxxMMWxWzM@W@x@WMWMMnz##+*z+i*zMnzxx#zn#zW*
+                               `xxz+##zz##xnnWWMnMnz#MM#zzMMMWWWMMWMW@WWnnWxn@z#Mz#i###***zx#z#znnWx;
+                               `*+n#z##z#zznnMWx#nxnMWz+#z*WW@#MxxnWMMMWxWWMMnxWz#n*+nzz+#*z##nnM@i
+                                :***###WW*zz*MxWMn+n*z#zzz*+xW@nx@xWM##xxznn*nz#WWzn#xnzzn#z+zzMM*`
+                                :+*M#+i##z;nz#x*n**+zi*#z+z+zzx@z@W@M#++#iiz+#n+i*zM@@Mxn*zxxWWM;
+*/
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+struct Rule {
+    char c = '\0';
+    std::vector<std::vector<int>> subrules {};
+};
+
+using Ruleset = std::map<int, Rule>;
+
+std::vector<std::string> match_rule(std::string message, const Ruleset& rules, int index);
+std::vector<std::string> match_rule_list(std::string message, const Ruleset& rules, const std::vector<int>& list);
+
+std::vector<std::string> split_string(std::string s, const std::string delim = " ", std::vector<std::string> acc = {}) {
+    auto pos = s.find(delim);
+    if (pos != std::string::npos) {
+        acc.push_back(s.substr(0, pos));
+        return split_string(s.substr(pos + delim.length()), delim, acc);
+    } else {
+        acc.push_back(s);
+        return acc;
+    }
+}
+
+std::vector<std::string> match_rule_list(std::string message, const Ruleset& rules, const std::vector<int>& list) {
+    std::vector<std::string> inputs = { message };
+
+    for (auto subindex : list) {
+        std::vector<std::string> more_inputs;
+        for (const auto& input : inputs) {
+            for (const auto& rest : match_rule(input, rules, subindex)) {
+                more_inputs.push_back(rest);
+            }
+        }
+
+        inputs = more_inputs;
+
+        if (inputs.empty()) {
+            break;
+        }
+    }
+
+    return inputs;
+}
+
+std::vector<std::string> match_rule(std::string message, const Ruleset& rules, int index) {
+    auto rule = rules.at(index);
+    if (rule.c != '\0') {
+        if (message[0] == rule.c) {
+            return { message.substr(1) };
+        } else {
+            return {};
+        }
+    } else if (rule.subrules.size() == 1) {
+        return match_rule_list(message, rules, rule.subrules[0]);
+    } else {
+        std::vector<std::string> result;
+
+        for (const auto& rule_list : rule.subrules) {
+            for (const auto& input : match_rule_list(message, rules, rule_list)) {
+                result.push_back(input);
+            }
+        }
+
+        return result;
+    }
+}
+
+bool any_empty(const std::vector<std::string>& inputs) {
+    for (const auto& input : inputs) {
+        if (input.empty()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    std::ifstream input("input");
+    if (!input.is_open()) {
+        std::cerr << "Can't read puzzle input file.\n";
+        return -1;
+    }
+
+    std::map<int, Rule> rules;
+
+    std::string line;
+    while (std::getline(input, line) && !line.empty()) {
+        auto delim = line.find(": ");
+        int id = std::stoi(line.substr(0, delim));
+        auto rest = line.substr(delim + 2); // delimiter is 2 chars long
+
+        if (auto quotes = rest.find('"'); quotes != std::string::npos) {
+            rules[id] = { .c = rest[quotes + 1] };
+        } else {
+            std::vector<std::vector<int>> subrules;
+            for (const auto& subrules_raw : split_string(rest, " | ")) {
+                std::vector<int> subrule = {};
+                for (const auto& subrule_raw : split_string(subrules_raw)) {
+                    subrule.push_back(std::stoi(subrule_raw));
+                }
+                subrules.push_back(subrule);
+            }
+            rules[id] = { .subrules = subrules };
+        }
+    }
+
+    std::vector<std::string> messages;
+    while (std::getline(input, line)) {
+        messages.push_back(line);
+    }
+
+    input.close();
+
+    int part1 = std::count_if(messages.cbegin(), messages.cend(), [&rules](std::string message) {
+        auto inputs = match_rule(message, rules, 0);
+        return any_empty(inputs);
+    });
+
+    std::cout << "part 1: " << part1 << '\n';
+
+    rules[8] = { .subrules = { { 42 }, { 42, 8 } } };
+    rules[11] = { .subrules = { { 42, 31 }, { 42, 11, 31 } } };
+
+    int part2 = std::count_if(messages.cbegin(), messages.cend(), [&rules](std::string message) {
+        auto inputs = match_rule(message, rules, 0);
+        return any_empty(inputs);
+    });
+
+    std::cout << "part 2: " << part2 << '\n';
+
+    return 0;
+}

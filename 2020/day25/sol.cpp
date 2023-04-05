228cd9b1dab6f7a65522f6c1e6749c93e96fd2bd
bitt4 <64093718+bitt4@users.noreply.github.com>
2023-04-06 00:18:07 +0200

    day 25: use binary exponentiation
    faster, but still not the fastest

--- 2020/day25/sol.cpp
+++ 2020/day25/sol.cpp
@@ -5,20 +5,27 @@ using u64 = uint64_t;
 
 u64 transform(u64 subject_number, int loop_size, int mod) {
     u64 value = 1;
-    for (int i = 0; i < loop_size; ++i) {
-        value = (value * subject_number) % mod;
+    subject_number %= mod;
+
+    // binary exponentiation
+    while (loop_size > 0) {
+        if (loop_size & 1) {
+            value = value * subject_number % mod;
+        }
+        subject_number = subject_number * subject_number % mod;
+        loop_size >>= 1;
     }
 
     return value;
 }
 
 int bruteforce_card_loop_size(u64 card_pub_key, int mod) {
-    constexpr int card_subject_number = 7;
+    constexpr int CARD_SUBJECT_NUMBER = 7;
     int loop_size = 0;
 
     u64 value = 1;
     while (value != card_pub_key) {
-        value = (value * transform(card_subject_number, 1, mod)) % mod;
+        value = (value * CARD_SUBJECT_NUMBER) % mod;
         loop_size++;
     }
 
@@ -39,10 +46,10 @@ int main() {
     std::getline(input, line);
     u64 door_pub_key = std::stoull(line);
 
-    constexpr int mod = 20201227;
+    constexpr int MOD = 20201227;
 
-    int card_loop_size = bruteforce_card_loop_size(card_pub_key, mod);
-    u64 encryption_key = transform(door_pub_key, card_loop_size, mod);
+    int card_loop_size = bruteforce_card_loop_size(card_pub_key, MOD);
+    u64 encryption_key = transform(door_pub_key, card_loop_size, MOD);
 
     std::cout << "part 1: " << encryption_key << '\n';
 

ae0fc55c3b50d3ebf4b37743b4064983228aa3ab
bitt4 <64093718+bitt4@users.noreply.github.com>
2023-04-15 20:35:55 +0200

    day 17: minor changes in c++ solution

--- 2020/day17/sol.cpp
+++ 2020/day17/sol.cpp
@@ -9,8 +9,11 @@ using Grid4d = std::array<std::array<std::array<std::array<char, 20>, 20>, 13>, 
 int get_nearby_cubes(const Grid4d& grid, v4 coords, bool four_dim = false) {
     auto [x, y, z, w] = coords;
 
+    int from = four_dim ? w - 1 : w;
+    int to = four_dim ? w + 1 : w;
+
     int count = 0;
-    for (int nw = (four_dim ? w - 1 : w); nw <= (four_dim ? w + 1 : w); ++nw) {
+    for (int nw = from; nw <= to; ++nw) {
         for (int nz = z - 1; nz <= z + 1; ++nz) {
             for (int ny = y - 1; ny <= y + 1; ++ny) {
                 for (int nx = x - 1; nx <= x + 1; ++nx) {
@@ -23,8 +26,7 @@ int get_nearby_cubes(const Grid4d& grid, v4 coords, bool four_dim = false) {
                     if (   nw >= std::ssize(grid)
                         || nz >= std::ssize(grid[0])
                         || ny >= std::ssize(grid[0][0])
-                        || nx >= std::ssize(grid[0][0][0])
-                        ) {
+                        || nx >= std::ssize(grid[0][0][0])) {
                         continue;
                     }
                     if (grid[nw][nz][ny][nx] == '#') {
@@ -38,26 +40,28 @@ int get_nearby_cubes(const Grid4d& grid, v4 coords, bool four_dim = false) {
     return count;
 }
 
-Grid4d cycle(Grid4d current_state, bool four_dim = false) {
+Grid4d cycle(const Grid4d& current_state, bool four_dim = false) {
     Grid4d next_state = {};
 
-    for (int w = (four_dim ? 0 : 6); w < (four_dim ? std::ssize(current_state) : 7); ++w) {
+    int from = four_dim ? 0 : 6;
+    int to = four_dim ? std::ssize(current_state) : 7;
+
+    for (int w = from; w < to; ++w) {
         for (int z = 0; z < std::ssize(current_state[0]); ++z) {
             for (int y = 0; y < std::ssize(current_state[0][0]); ++y) {
                 for (int x = 0; x < std::ssize(current_state[0][0][0]); ++x) {
-                    int nearby = get_nearby_cubes(current_state, {x, y, z, w}, four_dim);
+                    int nearby = get_nearby_cubes(current_state, { x, y, z, w }, four_dim);
+                    auto&& next_cube = next_state[w][z][y][x];
                     if (current_state[w][z][y][x] == '#') {
                         if (2 <= nearby && nearby <= 3) {
-                            next_state[w][z][y][x] = '#';
+                            next_cube = '#';
                         } else {
-                            next_state[w][z][y][x] = '.';
+                            next_cube = '.';
                         }
-                    } else if (current_state[w][z][y][x] != '#') {
-                        if (nearby == 3) {
-                            next_state[w][z][y][x] = '#';
-                        } else {
-                            next_state[w][z][y][x] = '.';
-                        }
+                    } else if (nearby == 3) {
+                        next_cube = '#';
+                    } else {
+                        next_cube = '.';
                     }
                 }
             }
@@ -69,7 +73,11 @@ Grid4d cycle(Grid4d current_state, bool four_dim = false) {
 
 int count_active(const Grid4d& grid, bool four_dim = false) {
     int count = 0;
-    for (int w = (four_dim ? 0 : 6); w < (four_dim ? std::ssize(grid) : 7); ++w) {
+
+    int from = four_dim ? 0 : 6;
+    int to = four_dim ? std::ssize(grid) : 7;
+
+    for (int w = from; w < to; ++w) {
         for (int z = 0; z < std::ssize(grid[0]); ++z) {
             for (int y = 0; y < std::ssize(grid[0][0]); ++y) {
                 for (int x = 0; x < std::ssize(grid[0][0][0]); ++x) {
